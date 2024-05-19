e138e9b88b6838e0077fd27bfbe2569a7a1e3620
“[theoVasilas]” <[theo.vasilas@gmail.com]>
2024-05-19 22:16:05 +0300

    update

--- client.py
+++ client.py
@@ -1,26 +1,28 @@
 from typing import Dict, Tuple
 from flwr.common import NDArrays
 import tensorflow as tf
-import flwr as fl 
+import flwr as fl
 
-model = tf.keras.applications.MobileNetV2((32,32,32), classes =10, weights =None)
-model.compile("adam", "sparse_categorocal_crossentropy", metrica=["accuracy"])
-(x_train, y_train), (x_test, y_test)= tf.keras.datasets.cifar10.load_data()
+# Correct input shape for MobileNetV2
+model = tf.keras.applications.MobileNetV2(input_shape=(32, 32, 32), classes=10, weights=None)
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(x_train, y_train, epochs=1, baych_size=32)
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+model.fit(x_train, y_train, epochs=1, batch_size=32)
 
 class FlowerClient(fl.client.NumPyClient):
-    def get_parameters(self, config: Dict[str, bool | bytes | float | int | str]) -> NDArrays[ndarray[Any, dtype[Any]]]:
-        return super().get_parameters(config)
+    def get_parameters(self, config):
+        return model.get_weights()
     
-    def fit(self, parametrs, config):
-        model.set_weights(parametrs)
-        model.fit(x_train, y_train, epoch=1, batch_size=32)
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=1, batch_size=32)
         return model.get_weights(), len(x_train), {}
     
     def evaluate(self, parameters, config):
-        model.se_weights(parameters)
+        model.set_weights(parameters)
         loss, accuracy = model.evaluate(x_test, y_test)
-        return loss, len(x_test), {"accuracy":accuracy}
-    
-fl.client.start_numpy_client(server_address="127.0.0.0:8888",client=FlowerClient)
+        return loss, len(x_test), {"accuracy": accuracy}
+
+fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=FlowerClient)
